b64cfd5427f7f2bcd6bccba1b6f5f43f53563367
Tony Rasa <trasa+github@meancat.com>
2014-09-02 11:19:34 -0700

    added exception info

--- ec-experiments/src/test/java/com/meancat/aws/ec/ObjectCacheIntegrationTest.java
+++ ec-experiments/src/test/java/com/meancat/aws/ec/ObjectCacheIntegrationTest.java
@@ -36,6 +36,31 @@ public class ObjectCacheIntegrationTest {
         }
     }
 
+    /*
+     The exception I see, perhaps due to timeout issues (vpc vs vpn vs vpc... remote to AWS...)
+
+     2014-09-02 11:05:44.032 ERROR net.spy.memcached.ConfigurationPoller:  Error encountered in the poller. Current cluster configuration: null
+     java.lang.RuntimeException: Exception waiting for config
+     at net.spy.memcached.MemcachedClient.getConfig(MemcachedClient.java:1668)
+     at net.spy.memcached.ConfigurationPoller.run(ConfigurationPoller.java:115)
+     at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:471)
+     at java.util.concurrent.FutureTask.runAndReset(FutureTask.java:304)
+     at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.access$301(ScheduledThreadPoolExecutor.java:178)
+     at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.run(ScheduledThreadPoolExecutor.java:293)
+     at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1145)
+     at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:615)
+     at java.lang.Thread.run(Thread.java:745)
+     Caused by: java.util.concurrent.ExecutionException: net.spy.memcached.internal.CheckedOperationTimeoutException: Operation timed out. - failing node: /10.11.110.221:11211
+     at net.spy.memcached.internal.OperationFuture.get(OperationFuture.java:106)
+     at net.spy.memcached.internal.GetConfigFuture.get(GetConfigFuture.java:50)
+     at net.spy.memcached.MemcachedClient.getConfig(MemcachedClient.java:1656)
+     ... 8 more
+     Caused by: net.spy.memcached.internal.CheckedOperationTimeoutException: Operation timed out. - failing node: /10.11.110.221:11211
+     ... 11 more
+     2014-09-02 11:05:44.045 WARN net.spy.memcached.ConfigurationPoller:  Number of consecutive poller errors is 1. Number of minutes since the last successful polling is 0
+
+     */
+
     @Test
     public void runManyTimes() throws InterruptedException, ExecutionException, TimeoutException, IOException {
         List<Long> timing = new ArrayList<>();
